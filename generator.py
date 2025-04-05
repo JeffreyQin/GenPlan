ef6d88910658d03899e6e24a224094854c628b7c
Jeffrey Qin <jeffreyqin612@gmail.com>
2025-04-05 14:22:13 -0400

    fixed observation algorithm in generator

--- generator.py
+++ generator.py
@@ -1,92 +1,97 @@
-from enum import Enum
+
 from collections import defaultdict
+from tree_builder import State, Cell, Action
 from copy import Copy
-
-
-class Cell(Enum):
-    WALL = 0
-    OBSERVED = 1
-    UNOBSERVED = 2
-    AGENT = 3
-    EXIT = 4
-
-class Action(Enum):
-    UP = 0
-    RIGHT = 1
-    DOWN = 2
-    LEFT = 2
-
-class State():
-    
-    def __init__(self, exit, agent_pos, observed):
-        self.exit = exit
-        self.agent_pos = agent_pos
-        self.observed = observed
 
 
 class Generator():
 
-    def __init__(self, map, range, reward):
+    def __init__(self, map: list[list], range: int, reward):
         
-        self.map:list[list] = map
+        self.map = map
         self.map_dims = map.shape
         self.range = range
         self.reward = reward
 
-        # create room index mapping
-        self.room_to_idx = defaultdict(lambda: None)
-        self.idx_to_room = defaultdict(lambda: None)
-        
-        curr_idx = 0
+        self.rooms = set()
         for r in range(self.map_dims[0]):
             for c in range(self.map_dims[1]):
-                if map[r, c] == Cell.OBSERVED.value or map[r, c] == Cell.UNOBSERVED.value or map[r, c] == Cell.AGENT.value:
-                    self.room_to_idx[(r, c)] = curr_idx
-                    self.idx_to_room[curr_idx] = (r, c)
-                    curr_idx += 1
-                elif map[r, c] == Cell.EXIT.value:
-                    self.exit = (r, c)
+                if map[r, c] != Cell.WALL.value:
+                    self.rooms.add((r, c))
 
-                    
 
-    def generate(self, state, action):
+    def is_observable(self, pos: tuple, target: tuple):
+        """
+        checks if target cell is observable from current cell
+
+        observable iff line connecting the centers of squares
+        does not contain a part or corner of a wall
+        """
+        r1, c1 = pos
+        r2, c2 = target
+
+        dr = r2 - r1
+        dc = c2 - c1
+
+        num_steps = max(abs(dr), abs(dc))
+        r_increment = dr / num_steps
+        c_increment = dc / num_steps
+        
+        r_curr = r1 + 0.5
+        c_curr = c1 + 0.5
+
+        for _ in range(num_steps):
+            r_curr += r_increment
+            c_curr += c_increment
+
+            # check if current square is wall
+            r_curr_coord, c_curr_coord = int(r_curr), int(c_curr)
+            if self.map[r_curr_coord, c_curr_coord] == Cell.WALL.value:
+                return False
+            
+            # check for pass through wall corner
+            r_next = r_curr + r_increment
+            c_next = c_curr + c_increment
+            r_next_coord, c_next_coord = int(r_next), int(c_next)
+            
+            if r_next_coord != r_curr_coord and c_next_coord != c_curr_coord:
+                if self.map[r_curr_coord, c_next_coord] == Cell.WALL.value or self.map[r_next_coord, c_curr_coord] == Cell.WALL.value:
+                    return False
+        return True
+    
+
+    def generate(self, state: State, action: int):
+        """
+        runs black box generator by performing input action on input state
+
+        returns new state, observation, and reward
+        """
 
         if action == Action.UP.value:
-            destination = (state.agent_pos[0] - 1, state.agent_pos[1])
+            dest = (state.agent[0] - 1, state.agent[1])
         elif action == Action.RIGHT.value:
-            destination = (state.agent_pos[0], state.agent_pos[1] + 1)
+            dest = (state.agent[0], state.agent[1] + 1)
         elif action == Action.DOWN.value:
-            destination = (state.agent_pos[0] + 1, state.agent_pos[1])
+            dest = (state.agent[0] + 1, state.agent[1])
         elif action == Action.LEFT.value:
-            destination = (state.agent_pos[0], state.agent_pos[1] - 1)
-
-        # if destination is wall or out of bound
-        # same state, no observation, no reward
-        if self.room_to_idx[destination] is None:
-            return state, set(), 0
+            dest = (state.agent[0], state.agent[1] - 1)
         
-        new_state = State(exit, destination, Copy(state.observed))
-
+        new_state = State(exit, dest, Copy(state.observed))
+        
         # summarize new observed cells
         observation = set()
-        found_exit = False
-        for d in range(1, self.range + 1):
-            cells = [
-                (destination[0] - d, destination[1]),
-                (destination[0], destination[1] + d),
-                (destination[0] + d, destination[1]),
-                (destination[0], destination[1] - d)
-            ]
-            for cell in cells:
-                if self.room_to_idx[cell] is not None and cell not in state.observed:
-                    observation.add(cell)
-                    new_state.observed.add(cell)
-                if cell == self.exit:
-                    found_exit = True
-            
-        if found_exit:
+        for room in self.rooms:
+            if self.is_observable(dest, room):
+                if room not in state.observed:
+                    new_state.observed.add(room)
+                    observation.add(room)
+                if room == state.exit:
+                    new_state.exit_found = True
+        
+        if new_state.exit_found:
             return new_state, observation, self.reward
         else:
-            return new_state, observation, 0
+            # penalize for step
+            return new_state, observation, -1
 
 
--- tree_builder.py
+++ tree_builder.py
@@ -1,17 +1,26 @@
 from enum import Enum
 
+class Cell(Enum):
+    WALL = 0
+    OBSERVED = 1
+    UNOBSERVED = 2
+    AGENT = 3
+    EXIT = 4
+
+class Action(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+class State():
+    def __init__(self, exit: tuple, agent: tuple, observed: set):
+        self.exit = exit
+        self.agent = agent
+        self.observed = observed
+        self.exit_found = False
+
+
 class TreeNode():
 
-    def __init__(self, generator, node_idx, parent_idx = -1):
-
-        self.generator = generator
-
-        self.node_idx = node_idx
-        self.parent_idx = parent_idx
-        self.is_root = True if parent_idx == -1 else False
-        
-        self.N = 
-        self.V = 
-        self.B = 
-        self.children = dict()
-        
+    def __init__(self):
