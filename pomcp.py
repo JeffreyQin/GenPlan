7aa6d575b488c24ba18753de857cd8ee915ceeab
Albear-Yang <albert.yang727@gmail.com>
2025-04-05 20:22:58 -0400

    started POMCP skeleton

---
+++ pomcp.py
@@ -0,0 +1,94 @@
+import random
+import math
+from generator import * #Figure this out later
+# #chat let me lock in rq
+
+
+class Node:
+    """
+    Class Node for pomcp
+    """
+    def __init__(self, obs, num_visited = 0, value = 0):
+        self.num_visited:int = num_visited
+        self.value:float = value
+        self.obs:set[tuple[int, int]] = obs
+        self.children:list[Node] = [] #empty for now
+
+
+epsilon:float = 0.001 #cut off for the discount factor
+tree:set[set[tuple[int, int]]] = set()
+
+
+
+
+def rollout(node:Node, state: tuple[int, int], depth:int):
+    """
+    rollout function for exploring new actions/states
+    """
+    random_action = random.randint(0,3)
+    new_state, obs, reward = generate(state, random_action)
+    return reward + rollout(node.children[random_action], new_state, depth + 1)
+
+    
+
+def simulate(state:tuple[int, int], node:Node, depth:int) -> float:
+    """
+    simulate function for searching
+    """
+    if not node.obs in tree:
+        tree.add(node.obs)
+        for i in range(0,4):
+            node.children.append(Node(node.obs, 0,0))
+
+        return rollout(node, state, depth)
+    else:
+        c:float = 0.5
+        a0_value:float = node.children[0].value + c * math.sqrt(
+            math.log(node.num_visited)/node.children[0].num_visited)
+        a1_value:float = node.children[1].value + c * math.sqrt(
+            math.log(node.num_visited)/node.children[1].num_visited)
+        a2_value:float = node.children[2].value + c * math.sqrt(
+            math.log(node.num_visited)/node.children[2].num_visited)
+        a3_value:float = node.children[3].value + c * math.sqrt(
+            math.log(node.num_visited)/node.children[3].num_visited)
+        
+        action_values:list[float] = [a0_value, a1_value, a2_value, a3_value]
+        a:int = action_values.index(max(action_values)) # a is the action you will take
+
+        new_state, obs, reward = generate(state, node ,action)
+
+        reward = reward + simulate(new_state, node.children[a] ,depth + 1)
+
+        #should belief still stay the same?
+
+        node.num_visited += 1
+        node.children[a].num_visited += 1
+        node.children[a].value = node.children[a].value + (reward - node.children[a].value)/node.children[a].num_visited
+
+        return reward
+        
+
+def best_action_index(root:Node) -> int:
+    """
+    Small helper function to find the action with the largest value given the node
+    """
+    action_values:list[float] = [root.children[0].value, root.children[1].value, root.children[2].value, root.children[3].value]
+    best_action:int = action_values.index(max(action_values))
+    return best_action
+
+def search(root:Node) -> int:
+    """
+    Search will take a node and return an integer corresponding to the best action
+    """
+    depth:int = 5 #REMEMBER TO ASK COLE/MARTA OR JEFF ABOUT THIS
+    while(True): #REMEMBER TO ASK COLE/MARTA OR JEFF ABOUT THIS
+        state:tuple[int, int] = random.choice(root.obs)
+        simulate(state, root, depth)
+        break #REMEMBER TO REMOVE THIS
+
+    best_action:int = best_action_index(root)
+    
+    return best_action
+
+
+
