c084dba93da2cfdadfc095ebe0178c946519c8af
Albear-Yang <albert.yang727@gmail.com>
2025-07-07 22:27:45 -0400

    changed fragment_path to fragment_planning_2

--- modular_planner.py
+++ modular_planner.py
@@ -335,7 +335,8 @@ def modular_planning(map, fragment, copies):
         entrance = path[-1]
         copy, base_r, base_c = segmentation[entrance]
 
-        fragment_path = fragment_planning(subtrees, fragment, (base_r, base_c))
+        #fragment_path = fragment_planning(subtrees, fragment, (base_r, base_c))
+        fragment_path = fragment_planning_2(fragment, (base_r, base_c))
 
         ## convert in-fragment path to global map coordinates
         fragment_path = [fragment_to_map_coords(fragment, copy)[r, c]
